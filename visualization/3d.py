03e4839724f86d23eb8b5ee80998051ca114c383
skyline-nju <duanyu.nju@hotmail.com>
2018-07-15 17:27:11 +0800

    create dividing_drop.ipynb

--- visualization/3d.py
+++ visualization/3d.py
@@ -8,22 +8,24 @@ def load_h5(filename):
     f = h5py.File(filename, "r")
     phi = f['1']['Phi']
     t = f['1']['t']
-    X, Y, Z = np.mgrid[-48:48:192j, -48:48:192j, -48:48:192j]
+    # X, Y, Z = np.mgrid[-30:30:120j, -30:30:120j, -30:30:120j]
+    X, Y, Z = np.mgrid[-60:60:240j, -60:60:240j, -60:60:240j]
+    # X, Y, Z = np.mgrid[-48:48:192j, -30:30:120j, -30:30:120j]
     for i in range(t.size):
         mlab.contour3d(X, Y, Z, phi[i], contours=[0.5], transparent=True)
-        mlab.axes(xlabel="x", ylabel="y", zlabel="z")
         # mlab.pipeline.image_plane_widget(
         #     mlab.pipeline.scalar_field(phi[i]),
         #     plane_orientation="z_axes",
-        #     slice_index=96)
+        #     slice_index=60)
         # mlab.pipeline.image_plane_widget(
         #     mlab.pipeline.scalar_field(phi[i]),
         #     plane_orientation="x_axes",
-        #     slice_index=96)
+        #     slice_index=60)
         # mlab.pipeline.image_plane_widget(
         #     mlab.pipeline.scalar_field(phi[i]),
         #     plane_orientation="y_axes",
         #     slice_index=96)
+        mlab.axes(xlabel="x", ylabel="y", zlabel="z")
         mlab.outline()
         mlab.show()
     mlab.close(all=True)
@@ -39,8 +41,8 @@ def test_contour3d():
 
 
 if __name__ == "__main__":
-    os.chdir(r"D:\data\droplet")
-    load_h5("dct3d.eps_0.19.r0_4.5.turnover_0.7.h5")
+    os.chdir(r"D:\data\droplet\droplet_3d\large")
+    load_h5("dct3d.eps_0.2.prolate_0.r0_5.turnover_1.h5")
     # obj = test_contour3d()
 
     # mlab.show()
